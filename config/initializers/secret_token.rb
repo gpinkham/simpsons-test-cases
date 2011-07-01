cbb72bbe2ae2d20e640e70b441a619d634b215b4
Gary Pinkham <gary@thepinkhams6.com>
2011-07-01 08:52:50 -0400

    initial rails app no changes yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpsonsTestCases::Application.config.secret_token = 'fcb40b36bcd4c4f5c27a1f9001ff2aaf778e390d46cccd0ce64b71ee61cddf8f77956bad37531c7773b9d3f92fb2afcf581aed27fe3c1ec6978682b7de490df7'
